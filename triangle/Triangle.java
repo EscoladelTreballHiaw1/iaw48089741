2afabf94ba8814e792576cf825b54bf90f857b0d
iaw48089741 <iaw48089741@h05.informatica.escoladeltreball.org>
2016-03-08 10:14:19 +0100

    triangle 1

---
+++ triangle/Triangle.java
@@ -0,0 +1,8 @@
+/*
+ * ArraysUtils.java        1.0 08/03/2016
+ *
+ * Copyright 2016 Mey-ling Garcia <meygpons@gmail.com>
+ * 
+ * This is free software, licensed under the GNU General Public License v3.
+ * See http://www.gnu.org/licenses/gpl.html for more information.
+ */
